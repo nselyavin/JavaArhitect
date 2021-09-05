51879d5353dfee0dae97bd21818c900af34a6245
Nikita Selyavin <nselyavin@inbox.ru>
2021-09-05 14:30:35 +0300

    Practic 1

---
+++ src/Practic1/main.java
@@ -0,0 +1,76 @@
+package Practic1;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class main {
+
+    static class PingWritter implements Runnable{
+        private Object locker;
+
+        public PingWritter(Object locker) {
+            this.locker = locker;
+        }
+
+        @Override
+        public void run() {
+            int i = 0;
+            synchronized (locker) {
+                while(i < 100) {
+                    System.out.println("Ping");
+                    i++;
+                    locker.notifyAll();
+                    try {
+                        locker.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    static class PongWritter implements Runnable{
+        private Object locker;
+
+        public PongWritter(Object locker) {
+            this.locker = locker;
+        }
+        @Override
+        public void run() {
+            int i = 0;
+            synchronized (locker) {
+                while(i < 100) {
+
+                    System.out.println("Pong");
+                    i++;
+
+                    locker.notifyAll();
+
+                    try {
+                        locker.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Object locker = new Object();
+        PingWritter pingWritter = new PingWritter(locker);
+        PongWritter pongWritter = new PongWritter(locker);
+        Thread th = new Thread(pingWritter);
+        Thread th2 = new Thread(pongWritter);
+
+        th.start();
+        th2.start();
+
+
+        System.out.println("puk");
+    }
+
+}
